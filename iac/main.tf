1285f538cba4eb69ad618b8b04ab4ead23e20826
Joel <mizukaze_010@hotmail.com>
2025-11-03 00:29:11 -0600

    feat: add s3 and apigateway initial configuration
    ch<nge: update devcontainer settings

--- iac/main.tf
+++ iac/main.tf
@@ -20,12 +20,24 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
-    s3          = "http://localhost:4566"
-    iam         = "http://localhost:4566"
-    amplify     = "http://localhost:4566"
-    ecs         = "http://localhost:4566"
-    lambda      = "http://localhost:4566"
-    eventbridge = "http://localhost:4566"
+    s3               = "http://172.18.0.3:4566"
+    iam              = "http://172.18.0.3:4566"
+    amplify          = "http://172.18.0.3:4566"
+    ecs              = "http://172.18.0.3:4566"
+    lambda           = "http://172.18.0.3:4566"
+    eventbridge      = "http://172.18.0.3:4566"
+    apigatewayv2     = "http://172.18.0.3:4566"
+    apigateway       = "http://172.18.0.3:4566"
+    cloudwatch       = "http://172.18.0.3:4566"
+    cloudwatchlogs   = "http://172.18.0.3:4566"
+    cloudwatchlog    = "http://172.18.0.3:4566"
+    cloudwatchevents = "http://172.18.0.3:4566"
+    events           = "http://172.18.0.3:4566"
+    sts              = "http://172.18.0.3:4566"
+    route53          = "http://172.18.0.3:4566"
+    route53domains   = "http://172.18.0.3:4566"
+    route53profiles  = "http://172.18.0.3:4566"
+    route53resolver  = "http://172.18.0.3:4566"
+    kms              = "http://172.18.0.3:4566"
   }
 }
-
---
+++ iac/s3.tf
@@ -0,0 +1,28 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "nimbus-drive"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "nimbus-drive-thumbnails"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
