fe4cc2dd98c8fa6c1210b7cead1cd69cc855326b
Joel Lopez J <mizukaze_010@hotmail.com>
2026-01-30 02:33:01 +0000

    feat(shared) :  add shared library to centralize common logic
    refactor(rag.query): update  and  to use the new  library for handling configurations and vector database connections.

--- rag-service/rag.vectorize/CHANGELOG.md
+++ rag-service/rag.vectorize/CHANGELOG.md
@@ -9,3 +9,8 @@ All notable changes to this project will be documented in this file.
 
 ### Changed
 - Update devcontainer settings
+
+## [0.2.0] - 2026-01-29
+### Changed
+- Refactor service to use `shared` library for vector database connection.
+- Updated `pyproject.toml` to include `shared` dependency.
---
+++ shared/src/shared/config.py
@@ -0,0 +1,36 @@
+import os
+from typing import Annotated
+
+from pydantic_settings import SettingsConfigDict
+from pydantic_settings_aws import ParameterStoreBaseSettings
+
+
+class AppSettings(ParameterStoreBaseSettings):
+
+    model_config = SettingsConfigDict(
+        aws_region=os.getenv("AWS_REGION", "us-east-1"),
+    )
+
+    mongodb_uri: Annotated[str, "/nimbus/app/mongo_uri"]
+    mongodb_dbname: Annotated[str, "/nimbus/app/mongodb_dbname"]
+    mongodb_collection: Annotated[str, "/nimbus/app/mongodb_collection"]
+    mongodb_vector_index: Annotated[str, "/nimbus/app/mongodb_vector_index"]
+
+    google_api_key: Annotated[str, "/nimbus/app/google_api_key"]
+    google_model_embedding: Annotated[str,
+                                      "/nimbus/app/google_model_embedding"]
+    google_model_chat: Annotated[str, "/nimbus/app/google_model_chat"]
+
+
+class SupaBaseSettings(ParameterStoreBaseSettings):
+
+    model_config = SettingsConfigDict(
+        aws_region=os.getenv("AWS_REGION")
+    )
+    supabase_url: Annotated[str, "/nimbus/supabase/supabase_url"]
+    supabase_anon_key: Annotated[str, "/nimbus/supabase/supabase_anon_key"]
+    supabase_jwt_secret: Annotated[str, "/nimbus/supabase/jwt_secret"]
+
+
+app_settings = AppSettings()
+supabase_settings = SupaBaseSettings()
